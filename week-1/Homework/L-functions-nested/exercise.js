2441ef7d5af33be3c4350cc8b37b06d1436cbef2
bipasha39 <physiobipasha39@gmail.com>
2020-08-17 21:16:52 +0200

    home l1

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,13 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function mentors(name){
+    let uppercase = name.toUpperCase();
+    return "HELLO " + uppercase;
+}
+console.log(mentors(mentor1));
+console.log(mentors(mentor2));
+console.log(mentors(mentor3));
+console.log(mentors(mentor4));
+console.log(mentors(mentor5));
