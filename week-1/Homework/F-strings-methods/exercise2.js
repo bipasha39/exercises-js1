5a66da0020fcaf46fd2891a3e9a2f57fc9dfd761
bipasha39 <physiobipasha39@gmail.com>
2020-08-17 15:13:52 +0200

    home work f2

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
-const name = " Daniel  ";
+const name = "Daniel";
+    const nameLength = 6;
+    const message = `     My name is ${name} , my name is ${nameLength} characters long      `;
 
-console.log(message);
+         console.log(message.trim());
