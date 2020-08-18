80d2f53858f8ffab49c68fb707c1c8fa5858a0df
bipasha39 <physiobipasha39@gmail.com>
2020-08-18 13:45:11 +0200

    homework k3

--- week-1/Homework/K-functions-parameters/exercise3.js
+++ week-1/Homework/K-functions-parameters/exercise3.js
@@ -9,9 +9,16 @@
   
 //   console.log(createGreeting(name));
 
+// function createGreeting(name){
+//     let name ="Daniel"
+// return "Hello my name is " +name;
+// }
+// //let greeting = createGreeting("Daniel");
+// console.log(createGreeting(name));
+
+
 function createGreeting(name){
-    let name="Daniel"
-return "Hello my name is " +name;
-}
-//let greeting = createGreeting("Daniel");
-console.log(createGreeting(name));
+    return "Hello my name is " +name;
+    }
+    console.log(createGreeting("Daniel"));
+    
