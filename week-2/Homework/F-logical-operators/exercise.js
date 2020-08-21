e087ef5a253c964ba7340b1e83317c17a690fdd8
bipasha39 <physiobipasha39@gmail.com>
2020-08-21 16:47:34 +0200

    homework F1

--- week-2/Homework/F-logical-operators/exercise.js
+++ week-2/Homework/F-logical-operators/exercise.js
@@ -6,19 +6,50 @@
 */
 
 // Do not change these two statement
-var htmlLevel = 8;
-var cssLevel = 4;
+let htmlLevel = 8;
+let cssLevel = 4;
 
 // Finish the statement to check whether HTML, CSS knowledge are above 5
 // (hint: use the comparison operator from before)
-var htmlLevelAbove5;
-var cssLevelAbove5;
+let htmlLevelAbove5;
+let cssLevelAbove5;
+
+  if (htmlLevel > 5) {
+    htmlLevelAbove5 = true;
+  }
+  else {
+    htmlLevelAbove5 = false;
+  }
+
+  if (cssLevel > 5) {
+    cssLevelAbove5 = true;
+  }
+  else {
+    cssLevelAbove5 = false;
+  }
+
+
+
+
+
 
 // Finish the next two statement
 // Use the previous variables and logical operators
 // Do not "hardcode" the answers
-var cssAndHtmlAbove5;
-var cssOrHtmlAbove5;
+let cssAndHtmlAbove5;
+let cssOrHtmlAbove5;
+
+if ( htmlLevel > 5 && cssLevel > 5){
+  cssAndHtmlAbove5 = true;
+} else {
+  cssAndHtmlAbove5 = false;
+}
+
+if ( htmlLevel > 5 || cssLevel > 5){
+  cssOrHtmlAbove5 = true;
+} else {
+  cssOrHtmlAbove5 = false;
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
