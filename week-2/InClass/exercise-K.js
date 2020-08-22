e72de85497bdf0b1904fff67fed73e23045193bc
bipasha39 <physiobipasha39@gmail.com>
2020-08-22 13:03:45 +0200

    inclass k

--- week-2/InClass/exercise-K.js
+++ week-2/InClass/exercise-K.js
@@ -1,7 +1,13 @@
 const nameOfStudents = ["A","B","C","D","E"];
   
-  for (let i = 0; i < nameOfStudents.length; i++) {
-    const nameMessage = "name is: " + nameOfStudents[i];
-    const indexMessage = "index is: " + i;
-    console.log(indexMessage, nameMessage);
-  }
+function students(array){ 
+        for (let i = 0; i < nameOfStudents.length; i++) {
+            const nameMessage = "name is: " + nameOfStudents[i];
+            const indexMessage = "index is: " + i;
+            console.log(indexMessage, nameMessage);
+        }
+}
+
+students(nameOfStudents);
+
+// i= index
