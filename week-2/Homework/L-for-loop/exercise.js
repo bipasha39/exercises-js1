7ca39812561c84fc1e4ed55d07bf70082c8bee1c
bipasha39 <physiobipasha39@gmail.com>
2020-08-22 14:06:13 +0200

    homework L

--- week-2/Homework/L-for-loop/exercise.js
+++ week-2/Homework/L-for-loop/exercise.js
@@ -9,7 +9,11 @@
 let n = 10;
 
 function sumTillNum(num){
-	//your code here
+	let summ = 0;
+	for (let i = 0; i <= num; i++) {
+		summ += i;
+	}
+	return summ;   //your code here
 }
 
 console.log("Sum from 0 to " + n + " is: " + sumTillNum(n));
