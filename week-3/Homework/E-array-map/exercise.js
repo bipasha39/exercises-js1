e27b92fc589932d68c864401d6539e714c851fee
bipasha39 <physiobipasha39@gmail.com>
2020-08-28 19:04:43 +0200

    homework E

--- week-3/Homework/E-array-map/exercise.js
+++ week-3/Homework/E-array-map/exercise.js
@@ -1,5 +1,8 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+const numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+const map1 = numbers.map(x => x * 100);
+
+console.log(map1);
