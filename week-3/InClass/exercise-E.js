5d2d88e9e6acaa9f5387ec15c2001033c959944a
bipasha39 <physiobipasha39@gmail.com>
2020-08-28 18:25:39 +0200

    inclass E

---
+++ week-3/InClass/exercise-E.js
@@ -0,0 +1,12 @@
+
+  const years = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
+  let yearsOfAll = years.map((year)=>{
+    return 2020-year;
+    });
+      console.log(yearsOfAll);
+      console.log(years);
+
+
+    
+      
+      
