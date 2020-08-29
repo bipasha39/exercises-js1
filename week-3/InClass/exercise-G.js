5d18edb59fc78087852c0c810a70b8c4f47e7cd3
bipasha39 <physiobipasha39@gmail.com>
2020-08-29 12:07:42 +0200

    inclass G

--- week-3/InClass/exercise-G.js
+++ week-3/InClass/exercise-G.js
@@ -1,9 +1,19 @@
-let arr = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
-
-function filterYears(listOfYears) {
-  let listFiltredYears = listOfYears.filter((item) => { return (2020 - item) > 17; });
-  console.log(`These are the birth years of people who can drive:${listFiltredYears.join()}`);
-  return listFiltredYears;
+const years = [ 1964, 2008, 1999, 2005, 1978, 1985, 1919 ];
+function canDrive(year) {
+    const age = 2020 - year;
+    if (age >= 17) {
+        return true
+    } else {
+        return false
+    }
 }
+const result = (`These are the birth years of people who can drive ` + years.filter(canDrive));
+console.log(result);
 
-console.log(filterYears(arr));
+
+
+
+
+
+
+
