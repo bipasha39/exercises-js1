401f850174a4ad900d15dafd85995ac00a81ecb4
bipasha39 <physiobipasha39@gmail.com>
2020-08-29 11:20:24 +0200

    inclass F

--- week-3/InClass/exercise-F.js
+++ week-3/InClass/exercise-F.js
@@ -1,15 +1,21 @@
 
-    const years = [1964, 2008, 1999, 2005, 1978, 1985, 1919];
-    let yearsOfAll = years.map((year)=>{
-      return 2020-year;
-      });
-        console.log(yearsOfAll);
-      
-      function isDrive(year) {
+      function canDrive(birthYear) {
+        const age = 2020 - birthYear;
+        if (age >= 17) {
+            return `Born in ${birthYear} can drive`;
+        } else {
+            const pendingYears = 17 - age;
+            return `Born in ${birthYear} can drive in ${pendingYears} years`;
+        }
+    }
+    // canDrive(1964)
+    // canDrive(2008)
+    // canDrive(1999)
+    const years = [ 1964, 2008, 1999, 2005, 1978, 1985, 1919 ];
+    const result = years.map(canDrive);
+    console.log(result);
+   
     
-        console.log((2020 - year) >= 17) ? `Born in ${year} candrive` : `Born in ${year} can drive in ${year + 17} year`);
-      }
-      // console.log(isDrive(2000));
-      // console.log(isDrive(2010));
-      console.log(yearsOld(years));
-          
+    
+    
+    
